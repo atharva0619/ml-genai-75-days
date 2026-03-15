d72a74f40f4c3e7c3ab50713c8244716bfd8e294
Atharva Mishra <atharva1960@gmail.com>
2026-03-16 00:27:41 +0530

    Day 1: Python basics practice
    Practiced Python variables, lists, and list comprehension.

---
+++ day01_python_basics/python_basics.py
@@ -0,0 +1,17 @@
+# Day 1 Python Basics Practice
+
+# Variables
+name = "Atharva"
+age = 20
+
+print("Name:", name)
+print("Age:", age)
+
+# List
+numbers = [1,2,3,4,5]
+
+# Square numbers
+squared = [x**2 for x in numbers]
+
+print("Numbers:", numbers)
+print("Squared:", squared)
